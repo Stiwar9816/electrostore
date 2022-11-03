21d5cb54efe33f634928ea319e885915262cccc3
JhonE17 <jhonepalacios17@gmail.com>
2022-11-03 14:08:27 -0500

    se agrego navbar y footer

--- login.php
+++ login.php
@@ -1,35 +1,44 @@
+<?php
+$active_sesion = 'active';
+
+?>
 <!doctype html>
 <html lang="es">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Electro TV</title>
-    <!-- CSS Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous" />
-    <link rel="stylesheet" href="./src/assets/css/sigin.css">
-</head>
+<!-- Head -->
+<?php include_once('src/components/head.php') ?>
+<!-- End Head -->
 
-<body class="text-center">
-    <main class="form-signin w-100 m-auto">
-        <form>
-            <img class="mb-4" src="./src/assets/img/logo.png" alt="" width="160" height="140">
-            <h1 class="h3 mb-3 fw-normal">Inicio de sesión</h1>
-            <div class="form-floating">
-                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                <label for="floatingInput">Usuario</label>
-            </div>
-            <div class="form-floating">
-                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                <label for="floatingPassword">Contraseña</label>
-            </div>
+<body class="text-center form-center">
+    <!-- Navbar -->
+    <?php include_once('src/components/navbar.php') ?>
+    <!-- End Navbar -->
+    <div class="container">
+        <div class="my-5 text-center">
+            <main class="form-signin w-150 m-auto">
+                <form>
+                    <img class="mb-4" src="./src/assets/img/logo.png" alt="" width="160" height="140">
+                    <h1 class="h3 mb-3 fw-normal">Inicio de sesión</h1>
+                    <div class="form-floating">
+                        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                        <label for="floatingInput">Usuario</label>
+                    </div>
+                    <div class="form-floating">
+                        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                        <label for="floatingPassword">Contraseña</label>
+                    </div>
 
-            <button class="w-100 btn btn-lg btn-primary mb-3" type="submit">Iniciar Sesión</button>
-            <a href="./register.php">No tienes una cuenta? Registrtate aquí!</a>
-            <a href="./index.php">Volver a la pagina principal</a>
-            <p class="mt-5 mb-3 text-muted">&copy; 2022</p>
-        </form>
-    </main>
+                    <button class="w-100 btn btn-lg btn-primary mb-3" type="submit">Iniciar Sesión</button>
+                    <a href="./register.php">No tienes una cuenta? Registrtate aquí!</a>
+                    <a href="./index.php">Volver a la pagina principal</a>
+                </form>
+            </main>
+
+            <!-- Footer -->
+            <?php include_once('src/components/footer.php') ?>
+            <!-- End Footer -->
+        </div>
+    </div>
     <!-- Scripts JS Bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
