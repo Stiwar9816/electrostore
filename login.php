0f9a773c7b40bbb0d71849803b9394ade0f580c9
Stiwar9816 <stiwar.asprilla1998@gmail.com>
2022-11-03 14:21:24 -0500

    fix routes css

--- login.php
+++ login.php
@@ -1,14 +1,7 @@
 <!doctype html>
 <html lang="es">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Electro TV</title>
-    <!-- CSS Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous" />
-    <link rel="stylesheet" href="./src/assets/css/sigin.css">
-</head>
+<?php include_once('./src/components/head_admin.php') ?>
 
 <body class="text-center">
     <main class="form-signin w-100 m-auto">
